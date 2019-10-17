b0d0a8a9a0197143165217b6782581049ce68976
mwasswa <m_wasswa@yahoo.com>
2019-10-17 15:48:06 -0400

    Updates to team controller
    Updated team controller to inherit from Protected controller
    Tested curl script to make sure it works
    Added user ownership to team controller to make sure users can only have
    access to what's in their account

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class TeamsController < ApplicationController
+class TeamsController < ProtectedController
   before_action :set_team, only: %i[show update destroy]
 
   # GET /teams
   def index
-    @teams = Team.all
+    @teams = current_user.teams
 
     render json: @teams
   end
@@ -17,7 +17,7 @@ class TeamsController < ApplicationController
 
   # POST /teams
   def create
-    @team = Team.new(team_params)
+    @team = current_user.teams.build(team_params)
 
     if @team.save
       render json: @team, status: :created, location: @team
@@ -44,7 +44,7 @@ class TeamsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_team
-    @team = Team.find(params[:id])
+    @team = current_user.teams.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
